9f182ec797d64b7e1c1c43fa63b0586db1034c92
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-04-29 19:01:04 +0800

    Nav button working but not in desktop

---
+++ js/nav.js
@@ -0,0 +1,36 @@
+`use strict`;
+
+const hamburgerCheckBox = document.getElementById("nav-toggle");
+const hamburgerBtn = document.getElementById("nav-hamburger");
+const exitBtn = document.getElementById("nav-exit");
+const mobileNav = document.querySelector(".nav-wrapper");
+// const mediaQ800 = window.matchMedia("(min-width: 800px)");
+
+const toggleShow = function (exitDisplay, borgarDisplay, navDisplay) {
+  exitBtn.style.display = exitDisplay;
+  hamburgerBtn.style.display = borgarDisplay;
+  mobileNav.style.marginLeft = navDisplay;
+};
+
+hamburgerBtn.addEventListener("click", function () {
+  toggleShow("block", "none", "0vw", mediaQ800);
+});
+
+exitBtn.addEventListener("click", function () {
+  toggleShow("none", "block", "-50vw", mediaQ800);
+});
+
+// const widthChange = function (widthC) {
+//   if (widthC.matches) {
+//     hamburgerBtn.addEventListener("click", function () {
+//       toggleShow("block", "none", "0vw");
+//     });
+
+//     exitBtn.addEventListener("click", function () {
+//       toggleShow("none", "block", "-50vw");
+//     });
+//   }
+// };
+
+// widthChange(mediaQ800);
+// mediaQ800.addEventListener("change", widthChange);
