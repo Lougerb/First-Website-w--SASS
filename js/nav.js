30cf6e0a92b7c029a79092d1c959e54f1a7cf876
LougerB <glouien@gmail.com>
2021-04-30 15:12:37 +0800

    Nav desktop and mobile fully working

--- js/nav.js
+++ js/nav.js
@@ -11,6 +11,7 @@ const toggleShow = function (exitDisplay, borgarDisplay, navDisplay) {
   hamburgerBtn.style.display = borgarDisplay;
   mobileNav.style.marginLeft = navDisplay;
 };
+
 const exitNav = function () {
   toggleShow("none", "block", "-50vw");
 };
@@ -19,15 +20,21 @@ hamburgerBtn.addEventListener("click", function () {
   toggleShow("block", "none", "0vw");
 });
 
-exitBtn.addEventListener("click", exitNav());
+// exitBtn.addEventListener("mouseup", exitNav());
 
 document.addEventListener("mouseup", function (e) {
-  if (mobileNav.contains(e.target)) {
-    return null;
+  const currentWidth = window.innerWidth;
+  if (currentWidth < 800) {
+    if (mobileNav.contains(e.target)) {
+      return null;
+    } else {
+      exitNav();
+    }
   } else {
-    exitNav();
+    return null;
   }
 });
+
 document.addEventListener("scroll", function (e) {
   if (mobileNav.contains(e.target)) {
     return null;
@@ -35,31 +42,51 @@ document.addEventListener("scroll", function (e) {
     exitNav();
   }
 });
-//ADD A FUNCTION WHEN DESKTOP MODE, NAV STYLE WILL BACK TO NORMAL
+
+// ADD A FUNCTION WHEN DESKTOP MODE, NAV STYLE WILL BACK TO NORMAL
+
 const desktopNav = function () {
   const currentWidth = window.innerWidth;
   if (currentWidth > 800) {
     mobileNav.style.marginLeft = "0vw";
   } else {
-    mobileNav.style.marginLeft = "-50vw";
+    exitNav();
   }
 };
-window.onresize = desktopNav;
-// document.addEventListener("change", desktopNav());
-//stage new 3
-//Sample commit
+// const desktopNav = function () {
+//   const currentWidth = window.innerWidth;
+//   if (currentWidth > 800) {
+//     mobileNav.style.marginLeft = "0vw";
 
-// const widthChange = function (widthC) {
-//   if (widthC.matches) {
+//     document.addEventListener("mouseup", function (e) {
+//       if (mobileNav.contains(e.target)) {
+//         return null;
+//       }
+//     });
+//   } else {
+//     mobileNav.style.marginLeft = "-50vw";
+
 //     hamburgerBtn.addEventListener("click", function () {
 //       toggleShow("block", "none", "0vw");
 //     });
 
-//     exitBtn.addEventListener("click", function () {
-//       toggleShow("none", "block", "-50vw");
+//     exitBtn.addEventListener("click", exitNav());
+
+//     document.addEventListener("mouseup", function (e) {
+//       if (mobileNav.contains(e.target)) {
+//         return null;
+//       } else {
+//         exitNav();
+//       }
+//     });
+
+//     document.addEventListener("scroll", function (e) {
+//       if (mobileNav.contains(e.target)) {
+//         return null;
+//       } else {
+//         exitNav();
+//       }
 //     });
 //   }
 // };
-
-// widthChange(mediaQ800);
-// mediaQ800.addEventListener("change", widthChange);
+window.onresize = desktopNav;
